312ea89668a3b0bac12690b36027620346bf5536
Ades <ades@binasanprima.com>
2023-11-23 08:43:03 +0700

    first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,21 @@
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Management System',
+    'description': 'Hospitl management system',
+    'depends': [''],
+    'data': [''],
+    'demo': [''],
+    'application':True,
+    'sequence':-1,
+    'installable': True,
+    'auto_install': False,
+    'assets':{},
+    'author': 'Ade Setiawan',
+    'website': '',
+    'license': 'LGPL-3',
+
+}
