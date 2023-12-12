745684b24139c6e9ce4b9930cce8dbcb2cfa21bf
Ades <37319154+digitalfarmer@users.noreply.github.com>
2023-12-12 14:52:03 +0700

    68. How To Inherit Model In Odoo __ Odoo Inheritance __ Odoo 15 Tutorials __ Odoo 15 Development

---
+++ om_odoo_inherit/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "om_odoo_inherit",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "digitalfarmer.github.io",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'tools',
+    'version': '0.1',
+    'sequence':1,
+    'application':True,
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
