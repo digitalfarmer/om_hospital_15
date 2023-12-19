20c30e7bec8785cb981fe89b46dc8ac35f3b0777
Ades <37319154+digitalfarmer@users.noreply.github.com>
2023-12-19 13:35:08 +0700

    70. How To Inherit A Function In Odoo __ Odoo Inheritance __ Odoo 15 Tutorials _ Odoo 15 Development

--- om_odoo_inherit/models/sale_order.py
+++ om_odoo_inherit/models/sale_order.py
@@ -7,3 +7,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     confirm_user_id= fields.Many2one('res.users', string='Confirmed User')
+
+    def action_confirm(self):
+        print("================success inherit function============") #task1
+        super(SaleOrder, self).action_confirm() #task2
+        self.confirm_user_id= self.env.user.id
