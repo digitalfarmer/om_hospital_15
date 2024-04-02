3b6bdfebd11542dd8540e9ff5bf9e9c0ac14c1de
Ades <37319154+digitalfarmer@users.noreply.github.com>
2024-04-02 08:59:08 +0700

    create sales order custom

---
+++ bis_sales_order/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Sales Order BSP',
+    'version': '1.0.0',
+    'category': 'Sales',
+    'summary': 'Manage sales Order BSP',
+    'description': 'Sales force for BSP',
+    'depends': ['base'],
+    'data':[
+
+
+    ],
+    'application':True,
+    'sequence':1,
+    'installable': True,
+    'auto_install': False,
+    'assets':{},
+    'author': 'Ade Setiawan',
+    'website': 'digitalfarmer.github.io',
+    'license': 'LGPL-3',
+
+}
